f79ec4690ea0c61d41b8d849d982418d021f048d
Gauravsingh Sisodia <xaerru@gmail.com>
2024-02-08 19:36:14 +0530

    feat: add nbit kmap

--- ckmap.py
+++ ckmap.py
@@ -19,65 +19,69 @@ class kmap:
     def isPowerOfTwo(self, x):
         return (x and (not(x & (x - 1))) )
 
-    def coor_to_num(self, x, y):
+    def coor_to_num(self, b, x, y):
+        b *= 16
         if x%2==0:
-            return self.binary_to_gray((x*4+y))
+            return b+self.binary_to_gray((x*4+y))
         else:
-            return self.binary_to_gray(x*4+(3-y))
+            return b+self.binary_to_gray(x*4+(3-y))
 
-    def make_groups(self, x, y):
-        if self.kmap[y][x]==0 or self.coor_to_num(x,y) in self.memo:
+    def make_groups(self, b, x, y):
+        if self.kmap[b][y][x]==0 or self.coor_to_num(b,x,y) in self.memo:
             return
-        xy = self.coor_to_num(x, y)
+        xy = self.coor_to_num(b,x, y)
 
-        if x+1==len(self.kmap[0]):
-            if self.kmap[y][0]==1:
-                self.groups.append([xy ,self.coor_to_num(0,y)])
-        else:
-            if self.kmap[y][x+1]==1:
-                self.groups.append([xy ,self.coor_to_num(x+1,y)])
-        if y+1==len(self.kmap):
-            if self.kmap[0][x]==1:
-                self.groups.append([xy ,self.coor_to_num(x,0)])
-        else:
-            if self.kmap[y+1][x]==1:
-                self.groups.append([xy ,self.coor_to_num(x,y+1)])
+        for i in range(len(self.kmap)):
+            self.groups.append([xy, self.coor_to_num(i, x, y)])
 
-        if x==0:
-            if self.kmap[y][len(self.kmap[0])-1]==1:
-                self.groups.append([xy ,self.coor_to_num(len(self.kmap[0])-1,y)])
+        if x+1==4:
+            if self.kmap[b][y][0]==1:
+                self.groups.append([xy ,self.coor_to_num(b,0,y)])
         else:
-            if self.kmap[y][x-1]==1:
-                self.groups.append([xy ,self.coor_to_num(x-1, y)])
-
-        if y==0:
-            if self.kmap[len(self.kmap)-1][x]==1:
-                self.groups.append([xy ,self.coor_to_num(x, len(self.kmap)-1)])
+            if self.kmap[b][y][x+1]==1:
+                self.groups.append([xy ,self.coor_to_num(b,x+1,y)])
+        if y+1==4:
+            if self.kmap[b][0][x]==1:
+                self.groups.append([xy ,self.coor_to_num(b,x,0)])
         else:
-            if self.kmap[y-1][x]==1:
-                self.groups.append([xy ,self.coor_to_num(x, y-1)])
-
-        self.memo.add(self.coor_to_num(x,y))
-
-        if x+1==len(self.kmap[0]):
-            self.make_groups(0, y)
-        else:
-            self.make_groups(x+1, y)
-
-        if y+1==len(self.kmap):
-            self.make_groups(x, 0)
-        else:
-            self.make_groups(x, y+1)
+            if self.kmap[b][y+1][x]==1:
+                self.groups.append([xy ,self.coor_to_num(b,x,y+1)])
 
         if x==0:
-            self.make_groups(len(self.kmap[0])-1, y)
+            if self.kmap[b][y][3]==1:
+                self.groups.append([xy ,self.coor_to_num(b,3, y)])
         else:
-            self.make_groups(x-1, y)
+            if self.kmap[b][y][x-1]==1:
+                self.groups.append([xy ,self.coor_to_num(b,x-1, y)])
 
         if y==0:
-            self.make_groups(x, len(self.kmap)-1)
+            if self.kmap[b][3][x]==1:
+                self.groups.append([xy ,self.coor_to_num(b,x,3)])
         else:
-            self.make_groups(x, y-1)
+            if self.kmap[b][y-1][x]==1:
+                self.groups.append([xy ,self.coor_to_num(b,x, y-1)])
+
+        self.memo.add(self.coor_to_num(b,x,y))
+
+        if x+1==4:
+            self.make_groups(b, 0, y)
+        else:
+            self.make_groups(b, x+1, y)
+
+        if y+1==4:
+            self.make_groups(b, x, 0)
+        else:
+            self.make_groups(b, x, y+1)
+
+        if x==0:
+            self.make_groups(b, 3, y)
+        else:
+            self.make_groups(b, x-1, y)
+
+        if y==0:
+            self.make_groups(b, x, 3)
+        else:
+            self.make_groups(b, x, y-1)
 
     def check_all_ones(self, subset):
         smemo = self.memo
@@ -114,12 +118,13 @@ class kmap:
         self.check_all_combinations(sg, c, i+1, j)
 
     def make_all_groups(self):
-        for x in range(4):
-            for y in range(4):
-                if self.coor_to_num(x, y) not in self.memo:
-                    self.make_groups(x, y)
-                if self.kmap[y][x]==1:
-                    self.groups.append([self.coor_to_num(x,y)]*2)
+        for b in range(len(self.kmap)):
+            for x in range(4):
+                for y in range(4):
+                    if self.coor_to_num(b,x, y) not in self.memo:
+                        self.make_groups(b, x, y)
+                    if self.kmap[b][y][x]==1:
+                        self.groups.append([self.coor_to_num(b,x,y)]*2)
 
     def get_minimal_groups(self):
         self.make_all_groups();
@@ -129,14 +134,14 @@ class kmap:
 
         m = copy.deepcopy(self.groups)
 
+        # print(self.groups)
         # for g in self.groups:
             # print(f"{g[0]}-{g[1]}")
         self.groups = [list(i) for i in set([tuple(sorted(x)) for x in list(nx.simple_cycles(G)) if self.isPowerOfTwo(len(x))])]
+        # print(self.groups)
 
         id = 0
-        while True:
-            if id==len(self.groups):
-                break
+        while id!=len(self.groups):
             g = self.groups[id]
             if len(g)==8:
                 o = []
@@ -149,6 +154,34 @@ class kmap:
                     o.append(c)
                 for x in o:
                     if x<3:
+                        self.groups.remove(g)
+                        id-=1
+                        break
+            if len(g)==16:
+                o = []
+                for i in g:
+                    c = 0
+                    for j in g:
+                        if i!=j:
+                            if [i,j] in m or [j, i] in m:
+                                c+=1
+                    o.append(c)
+                for x in o:
+                    if x<4:
+                        self.groups.remove(g)
+                        id-=1
+                        break
+            if len(g)==32:
+                o = []
+                for i in g:
+                    c = 0
+                    for j in g:
+                        if i!=j:
+                            if [i,j] in m or [j, i] in m:
+                                c+=1
+                    o.append(c)
+                for x in o:
+                    if x<5:
                         self.groups.remove(g)
                         id-=1
                         break
@@ -170,10 +203,16 @@ class kmap:
 
 
 if __name__=="__main__":
-    k = kmap([[1,1,1,1],
-              [1,1,1,1],
-              [1,1,1,0],
-              [1,0,1,1]])
+    k = kmap([[[1,1,1,1],
+               [1,1,1,1],
+               [1,1,1,0],
+               [1,0,1,1]],
+
+              [[1,1,0,1],
+               [1,1,0,1],
+               [1,0,0,0],
+               [1,0,1,1]],
+              ])
     print(k.get_minimal_groups())
     # k.make_all_groups()
     # # In each flood do the following
